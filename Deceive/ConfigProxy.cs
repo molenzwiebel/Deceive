41d5b3edc89f7ddddf7da6ecc82ee15a327bfdf9
Thijs Molendijk <info@thijsmolendijk.nl>
2019-12-03 22:58:27 +0100

    Add clientconfig proxy for PBE/OCE, bump version

---
+++ Deceive/ConfigProxy.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Deceive.Properties;
+using EmbedIO;
+using EmbedIO.Actions;
+
+namespace Deceive
+{
+    class ConfigProxy
+    {
+        private static readonly HttpClient Client = new HttpClient();
+
+        /**
+         * Starts a new client configuration proxy at a random port. The proxy will modify any responses
+         * to point the chat servers to our local setup. This function returns the random port that the HTTP
+         * server is listening on.
+         */
+        public static int StartConfigProxy(string configUrl, int chatPort)
+        {
+            // Find a free port.
+            var l = new TcpListener(IPAddress.Loopback, 0);
+            l.Start();
+            var port = ((IPEndPoint) l.LocalEndpoint).Port;
+            l.Stop();
+
+            // Start a web server that sends everything to ProxyAndRewriteResponse
+            var server = new WebServer(o => o
+                    .WithUrlPrefix("http://localhost:" + port)
+                    .WithMode(HttpListenerMode.EmbedIO))
+                .WithModule(new ActionModule("/", HttpVerbs.Get,
+                    ctx => ProxyAndRewriteResponse(configUrl, chatPort, ctx)));
+
+            // Run this on a new thread, just for the sake of it.
+            // It seemed to be buggy if run on the same thread.
+            new Thread(() =>
+            {
+                server.RunAsync().Wait();
+            }).Start();
+
+            return port;
+        }
+
+        /**
+         * Proxies any request made to this web server to the clientconfig service. Rewrites the response
+         * to have any chat servers point to localhost at the specified port.
+         */
+        private async static Task ProxyAndRewriteResponse(string configUrl, int chatPort, IHttpContext ctx)
+        {
+            var url = configUrl + ctx.Request.RawUrl;
+            Console.WriteLine(url);
+
+            using (var message = new HttpRequestMessage(HttpMethod.Get, url))
+            {
+                // Cloudflare bitches at us without a user agent.
+                message.Headers.TryAddWithoutValidation("User-Agent", ctx.Request.Headers["user-agent"]);
+
+                // Add authorization headers for player config.
+                if (ctx.Request.Headers["x-riot-entitlements-jwt"] != null)
+                {
+                    message.Headers.TryAddWithoutValidation("X-Riot-Entitlements-JWT",
+                        ctx.Request.Headers["x-riot-entitlements-jwt"]);
+                }
+
+                if (ctx.Request.Headers["authorization"] != null)
+                {
+                    message.Headers.TryAddWithoutValidation("Authorization", ctx.Request.Headers["authorization"]);
+                }
+
+                var result = await Client.SendAsync(message);
+                var content = await result.Content.ReadAsStringAsync();
+                var modifiedContent = content;
+
+                try
+                {
+                    var configObject = (JsonObject) SimpleJson.DeserializeObject(content);
+
+                    // Set chat.affinities (a dictionary) to all localhost.
+                    if (configObject.ContainsKey("chat.affinities"))
+                    {
+                        var affinities = (JsonObject) configObject["chat.affinities"];
+                        foreach (var key in new List<string>(affinities.Keys)) // clone to prevent concurrent modification
+                        {
+                            affinities[key] = "127.0.0.1";
+                        }
+                    }
+
+                    // Set fallback host to localhost.
+                    if (configObject.ContainsKey("chat.host"))
+                    {
+                        configObject["chat.host"] = "127.0.0.1";
+                    }
+
+                    // Set chat port.
+                    if (configObject.ContainsKey("chat.port"))
+                    {
+                        configObject["chat.port"] = chatPort;
+                    }
+
+                    // Allow an invalid cert.
+                    if (configObject.ContainsKey("chat.allow_bad_cert.enabled"))
+                    {
+                        configObject["chat.allow_bad_cert.enabled"] = true;
+                    }
+
+                    modifiedContent = SimpleJson.SerializeObject(configObject);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+
+                    // Show a message instead of failing silently.
+                    MessageBox.Show(
+                        "Deceive was unable to rewrite a League of Legends configuration file. This normally happens because Riot changed something on their end. Please check if there's a new version of Deceive available, or contact the creator through GitHub (https://github.com/molenzwiebel/deceive) or Discord if there's not.\n\n" +
+                        ex,
+                        Resources.DeceiveTitle,
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error,
+                        MessageBoxDefaultButton.Button1
+                    );
+
+                    Application.Exit();
+                }
+
+                // Using the builtin EmbedIO methods for sending the response adds some garbage in the front of it.
+                // This seems to do the trick.
+                var responseBytes = Encoding.UTF8.GetBytes(modifiedContent);
+
+                ctx.Response.StatusCode = (int) result.StatusCode;
+                ctx.Response.SendChunked = false;
+                ctx.Response.ContentLength64 = responseBytes.Length;
+                ctx.Response.ContentType = "application/json";
+                ctx.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
+                ctx.Response.OutputStream.Close();
+            }
+        }
+    }
+}
--- Deceive/Program.cs
+++ Deceive/Program.cs
@@ -6,6 +6,7 @@ using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Windows.Forms;
 using Deceive.Properties;
 using YamlDotNet.RepresentationModel;
@@ -54,6 +55,7 @@ namespace Deceive
 
                 if (result != DialogResult.Yes) return;
                 Utils.KillLCU();
+                Thread.Sleep(2000); // Riot Client takes a while to die
             }
 
             // Step 1: Open a port for our proxy, so we can patch the port number into the system yaml.
@@ -61,21 +63,21 @@ namespace Deceive
             listener.Start();
             var port = ((IPEndPoint)listener.LocalEndpoint).Port;
 
-            // Step 2: Find original system.yaml, patch our localhost proxy in, and save it somewhere.
+            // Step 2: Find original League system.yaml, patch our localhost proxy in, and save it somewhere.
             // At the same time, also parse the system.yaml to get the original chat server locations.
-            var sysYamlPath = Utils.GetSystemYamlPath();
-            if (sysYamlPath == null) // If this is null, it means we canceled something that required manual user input. Just exit.
+            var leagueSystemYamlPath = Utils.GetSystemYamlPath();
+            if (leagueSystemYamlPath == null) // If this is null, it means we canceled something that required manual user input. Just exit.
                 return;
 
-            var contents = File.ReadAllText(sysYamlPath);
+            var leagueYamlContents = File.ReadAllText(leagueSystemYamlPath);
 
             // Load the stream
-            var yaml = new YamlStream();
-            yaml.Load(new StringReader(contents));
+            var leagueYaml = new YamlStream();
+            leagueYaml.Load(new StringReader(leagueYamlContents));
 
-            contents = contents.Replace("allow_self_signed_cert: false", "allow_self_signed_cert: true");
-            contents = contents.Replace("chat_port: 5223", "chat_port: " + port);
-            contents = new Regex("chat_host: .*?\t?\n").Replace(contents, "chat_host: localhost\n");
+            leagueYamlContents = leagueYamlContents.Replace("allow_self_signed_cert: false", "allow_self_signed_cert: true");
+            leagueYamlContents = leagueYamlContents.Replace("chat_port: 5223", "chat_port: " + port);
+            leagueYamlContents = new Regex("chat_host: .*?\t?\n").Replace(leagueYamlContents, "chat_host: localhost\n");
 
             // Write this to the league install folder and not the appdata folder.
             // This is because league segfaults if you give it an override path with unicode characters,
@@ -83,40 +85,60 @@ namespace Deceive
             // We put it in the Config folder since the new patcher will nuke any non-league files in the install root.
             var leaguePath = Utils.GetLCUPath();
             var yamlPath = Path.Combine(Path.GetDirectoryName(leaguePath), "Config", "deceive-system.yaml");
-            File.WriteAllText(yamlPath, contents);
+            File.WriteAllText(yamlPath, leagueYamlContents);
 
-            // Step 3: Either launch Riot Client or launch League, depending on local configuration.
+            // Step 3: Find the Riot Client.
             var riotClientPath = Utils.GetRiotClientPath();
 
-            ProcessStartInfo startArgs;
-            if (riotClientPath != null && File.Exists(riotClientPath))
+            // If the riot client doesn't exist, the user is either severely outdated or has a bugged install.
+            if (riotClientPath == null)
             {
-                startArgs = new ProcessStartInfo
-                {
-                    FileName = riotClientPath,
-                    Arguments = "--priority-launch-pid=12345 --priority-launch-path=\"" + leaguePath + "\" -- --system-yaml-override=\"" + yamlPath + "\""
-                };
-            }
-            else
-            {
-                startArgs = new ProcessStartInfo
-                {
-                    FileName = leaguePath,
-                    Arguments = "--system-yaml-override=\"" + yamlPath + "\""
-                };
+                MessageBox.Show(
+                    "Deceive was unable to find the path to the Riot Launcher. If you have League installed and it is working properly, please file a bug report through GitHub (https://github.com/molenzwiebel/deceive) or Discord.",
+                    Resources.DeceiveTitle,
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question,
+                    MessageBoxDefaultButton.Button1
+                );
+
+                return;
             }
 
-            // Step 4: Start the process and wait for a connect.
+            // Step 4: Do a similar change to the Riot Client system.yaml, to patch out the client config.
+            var riotYamlPath = Path.Combine(Path.GetDirectoryName(riotClientPath), "system.yaml");
+            var riotYamlContents = File.ReadAllText(riotYamlPath);
+
+            // Find the old config URL (likely https://clientconfig.rpg.riotgames.com)
+            var riotYaml = new YamlStream();
+            riotYaml.Load(new StringReader(riotYamlContents));
+            var root = leagueYaml.Documents[0].RootNode;
+            var oldConfigUrl = root["region_data"][root["default_region"].ToString()]["servers"]["client_config"]["client_config_url"].ToString();
+
+            // Start a proxy web server and replace the yaml config URL with localhost.
+            var proxyServerPort = ConfigProxy.StartConfigProxy(oldConfigUrl, port);
+            riotYamlContents = new Regex("client_config_url: .*?\t?\n").Replace(riotYamlContents, "client_config_url: \"http://localhost:" + proxyServerPort + "\"\n");
+
+            // Write the modified system yaml back. We write it to the league folder since we know it exists.
+            var newRiotYamlPath = Path.Combine(Path.GetDirectoryName(leaguePath), "Config", "deceive-riot-client-system.yaml");
+            File.WriteAllText(newRiotYamlPath, riotYamlContents);
+
+            // Step 5: Start the process and wait for a connect.
+            var startArgs = new ProcessStartInfo
+            {
+                FileName = riotClientPath,
+                Arguments = "--system-yaml-override=\"" + newRiotYamlPath + "\" --launch-product=league_of_legends --launch-patchline=live -- --system-yaml-override=\"" + yamlPath + "\""
+            };
+
             Process.Start(startArgs);
             var incoming = listener.AcceptTcpClient();
 
-            // Step 5: Connect sockets.
+            // Step 6: Connect sockets.
             var sslIncoming = new SslStream(incoming.GetStream());
             var cert = new X509Certificate2(Resources.certificates);
             sslIncoming.AuthenticateAsServer(cert);
 
             // Find the chat information of the original system.yaml for that region.
-            var regionDetails = yaml.Documents[0].RootNode["region_data"][Utils.GetLCURegion()]["servers"]["chat"];
+            var regionDetails = leagueYaml.Documents[0].RootNode["region_data"][Utils.GetLCURegion()]["servers"]["chat"];
             var chatHost = regionDetails["chat_host"].ToString();
             var chatPort = int.Parse(regionDetails["chat_port"].ToString());
 
@@ -124,7 +146,7 @@ namespace Deceive
             var sslOutgoing = new SslStream(outgoing.GetStream());
             sslOutgoing.AuthenticateAsClient(chatHost);
 
-            // Step 6: All sockets are now connected, start tray icon.
+            // Step 7: All sockets are now connected, start tray icon.
             var mainController = new MainController();
             mainController.StartThreads(sslIncoming, sslOutgoing);
             Application.EnableVisualStyles();
