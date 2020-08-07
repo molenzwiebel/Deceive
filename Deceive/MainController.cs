5c1bcfb29465d4bd3cea00e78a680aca55aa9480
Cyrus <54488650+RealCyGuy@users.noreply.github.com>
2020-08-07 03:54:09 -0700

    it's "Right-click" not "Right-Click" (#72)

--- Deceive/MainController.cs
+++ Deceive/MainController.cs
@@ -36,7 +36,7 @@ namespace Deceive
                 Icon = Resources.DeceiveIcon,
                 Visible = true,
                 BalloonTipTitle = StartupHandler.DeceiveTitle,
-                BalloonTipText = "Deceive is currently masking your status. Right-Click the tray icon for more options."
+                BalloonTipText = "Deceive is currently masking your status. Right-click the tray icon for more options."
             };
             _trayIcon.ShowBalloonTip(5000);
 
